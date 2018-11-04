c075910f4c5294dfda5e0dd07621e461a3791485
Omer Faruk Yilmaz <omerfarukyilmaz93@gmail.com>
2018-11-04 19:45:39 +0800

    Expose ImageView and ProgressBar

--- imageviewprogress/src/main/java/com/ofylab/imageviewprogress/ImageViewProgress.kt
+++ imageviewprogress/src/main/java/com/ofylab/imageviewprogress/ImageViewProgress.kt
@@ -2,10 +2,18 @@ package com.ofylab.imageviewprogress
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.image_view_progress.view.*
 
 class ImageViewProgress(context: Context, attrs: AttributeSet) : RelativeLayout(context, attrs) {
+
+    val imageView: ImageView
+        get() = imageView_
+
+    val progressBar: ProgressBar
+        get() = progressBar_
 
     init {
         inflate(context, R.layout.image_view_progress, this)
